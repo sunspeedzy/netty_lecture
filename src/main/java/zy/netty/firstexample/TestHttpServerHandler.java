d10ea27148b6a00f36d60a9d25c095bee43cc2c8
sunspeedzy <84886255@qq.com>
2019-04-24 18:59:32 +0800

    Share Netty project to Github

---
+++ src/main/java/zy/netty/firstexample/TestHttpServerHandler.java
@@ -0,0 +1,23 @@
+package zy.netty.firstexample;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest){
+            ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK, content);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            ctx.writeAndFlush(response);
+        }
+    }
+}
---
+++ src/main/java/zy/netty/firstexample/TestServer.java
@@ -0,0 +1,31 @@
+package zy.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 客户端发出不带参数的请求，服务器端返回 Hello World
+ */
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        // EventLoop 本质是一个死循环，不命令退出，一直不会退出
+        // bossGroup完成连接的接收，workerGroup完成连接的处理
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
