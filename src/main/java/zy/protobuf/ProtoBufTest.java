15a924df5a828f219d3e83daa3cce5bf76e923d8
sunspeedzy <84886255@qq.com>
2019-05-15 10:25:59 +0800

    基于 15_Protobuf集成Netty与多协议消息传递 修改

---
+++ src/main/java/zy/protobuf/ProtoBufTest.java
@@ -0,0 +1,24 @@
+package zy.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class ProtoBufTest {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        DataInfo.Student student = DataInfo.Student.newBuilder()
+                .setName("张三").setAge(20).setAddress("北京")
+                .build();
+
+        byte[] student2ByteArray = student.toByteArray();  // 对象转换成 字节数组（序列化），字节数组内容可以在网络上传输
+
+        DataInfo.Student student2 = DataInfo.Student.parseFrom(student2ByteArray); // 把字节数组再转换为 对象（反序列化）
+
+        System.out.println(student2);
+//        输出结果有字符串的转义
+//        name: "\345\274\240\344\270\211"
+//        age: 20
+//        address: "\345\214\227\344\272\254"
+        System.out.println(student2.getName());
+        System.out.println(student2.getAge());
+        System.out.println(student2.getAddress());
+    }
+}
